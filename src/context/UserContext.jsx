1246bc6f61af259eed72c8ca170af9a037bc8814
Daniel Cardenas <felipe08f@gmail.com>
2023-03-13 11:49:40 -0500

    cracion archivo variables de entorno

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,20 +1,22 @@
-import { useState, createContext, useMemo } from "react";
+import { createContext } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
 
 
 export const UserContext = createContext();
 
 export function UserContextProvider(props) {
 
+    const URL_API = import.meta.env.VITE_API_URL
+
     function login(name, password) {
+        console.log(URL_API)
         axios.post('http://localhost:4000/api/auth/login', {
             name: name,
             password: password
         })
             .then((res) => {
-                localStorage.setItem('key',JSON.stringify(res.data.tokenSession))
-                localStorage.setItem('user',JSON.stringify(res.data.data))
+                localStorage.setItem('key', JSON.stringify(res.data.tokenSession))
+                localStorage.setItem('user', JSON.stringify(res.data.data))
                 window.location.assign('/home')
             })
             .catch((err) => {
