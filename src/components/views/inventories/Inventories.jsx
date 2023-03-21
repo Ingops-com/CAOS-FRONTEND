7c5d08d3707c725490c1e147f3f3154060c58ca1
niko5778 <lozanonicolas65@gmail.com>
2023-03-21 17:14:39 -0500

    integracion de variables de edicion en el Rawcontext

--- src/components/views/inventories/Inventories.jsx
+++ src/components/views/inventories/Inventories.jsx
@@ -1,16 +1,20 @@
-import { useContext, useEffect } from 'react';
-import { BsFillTrashFill, BsPencilFill } from 'react-icons/bs'
+import { useContext, useEffect, useState } from 'react';
+import { BsFillTrashFill, BsPlusCircle } from 'react-icons/bs'
 import { InventoriesContext } from '../../../context/InventoriesContext.jsx';
-import RawMateContextProvider from '../../../context/RawMateContext.jsx';
-import CategoriesContextProvider from '../../../context/CategoriesContext.jsx';
+import { RawMateContext } from '../../../context/RawMateContext.jsx';
 import Cards from '../../commons/cards/Cards.jsx'
 import RawMaterialForm from './RawMaterialForm/RawMaterialForm.jsx'
+import RawMaterialFormEdit from './RawMaterialForm/RawMaterialFormEdit.jsx'
 import './Inventories.css'
+
+
 
 
 function Inventories() {
 
     const { data, date, valTotalRawMate } = useContext(InventoriesContext)
+    const { setEditData } = useContext(RawMateContext)
+    const [active, setActive] = useState(false);
 
     return (
         <div className='inventoriesBody'>
@@ -32,12 +36,10 @@ function Inventories() {
                     cardNum='card3'
                 />
             </div>
-
-            <CategoriesContextProvider>
-                <RawMateContextProvider>
-                    <RawMaterialForm />
-                </RawMateContextProvider>
-            </CategoriesContextProvider>
+            <div className='transition-all' id='formEdit'>
+                {active ? (<RawMaterialFormEdit />) : (<></>)}
+            </div>
+            <RawMaterialForm />
 
             <div className='flex w-full items-center justify-center shadow-xl p-5 dark:shadow-none dark:bg-dark-ing-800'>
                 <div className=' w-full max-h-96 overflow-auto'>
@@ -52,15 +54,18 @@ function Inventories() {
                             </tr>
                         </thead >
                         <tbody >
-                            {data.map((materia, index) => (
-                                <tr key={index} className='text-center odd:bg-transparent even:bg-slate-200 dark:even:bg-dark-ing-700 dark:odd:bg-transparent dark:text-white'>
+                            {data.map((materia) => (
+                                <tr key={materia.id} className='text-center odd:bg-transparent even:bg-slate-200 dark:even:bg-dark-ing-700 dark:odd:bg-transparent dark:text-white'>
                                     <td>{materia.raw_material.name}</td>
                                     <td>{materia.stock}</td>
                                     <td>{materia.raw_material.unit_measure.name}</td>
                                     <td>{materia.price}</td>
                                     <td>
                                         <button className='bg-red-600 p-2 rounded-lg pr-4 pl-4 m-2'><BsFillTrashFill color='ffffff' /></button>
-                                        <button className='bg-yellow-500 p-2 rounded-lg pr-4 pl-4 m-2'><BsPencilFill color='ffffff' /></button>
+                                        <button className='bg-yellow-500 p-2 rounded-lg pr-4 pl-4 m-2' onClick={() => {
+                                            setActive(!active)
+                                            setEditData(materia)
+                                        }} ><BsPlusCircle color='ffffff' /></button>
                                     </td>
                                 </tr>)
                             )}
@@ -73,3 +78,4 @@ function Inventories() {
 }
 
 export default Inventories
+
